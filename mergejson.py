9622109aaa63c9768c4274311d65f21f788ee5c9
Anish M Raghavendra <39957296+amr-anish@users.noreply.github.com>
2019-11-06 20:01:23 +0530

    Add files via upload

---
+++ mergejson.py
@@ -0,0 +1,41 @@
+import json
+import os
+import jsonmerge as js
+
+schema = {'mergeStrategy': 'objectMerge',
+                  'properties': {
+                      'b': {'mergeStrategy': 'overwrite'}
+                  },
+                  'additionalProperties': {
+                      'mergeStrategy': 'append'
+                  }}
+
+base = None
+base = js.merge(base, {'a': ["a"]}, schema)
+base = js.merge(base, {'a': ["b"], 'b': 'c'}, schema)
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+#To Get folder Path
+folder_name=str(input("Folder Path :  [ Example : jsonfiles/ ] : "))
+print("Rules : Enter the required json ID one by one and click enter Type M to Merge the json ")
+
+while(True):
+    j_id=str(input(" Enter the json Data ID [eg: 1 or 2] or type  m to merge: "))
+    if(j_id=="m"or j_id=="M"):
+        break
+    my_file = os.path.join(THIS_FOLDER, folder_name+'data'+j_id+'.json' )
+    if(int(j_id)==1):
+        with open(my_file) as json_file:
+             a = json.load(json_file)
+    else:
+        with open(my_file) as json_file:
+            b = json.load(json_file)
+            a = js.merge(b, a, schema) 
+
+
+
+print(a)
+
+print("Output file also created see in the root folder ")
+with open('output.json', 'w') as f:
+  json.dump(a, f, ensure_ascii=False)
